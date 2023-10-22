3a84f6aebcabda4505b4ea3e3e2cdb94b8f8198a
fsentypal <fsentypal13@gmail.com>
2023-10-22 16:30:53 -0500

    update furniture

--- CamelotJava/src/com/entities/Furniture.java
+++ CamelotJava/src/com/entities/Furniture.java
@@ -1,5 +1,14 @@
 package com.entities;
 
-public class Furniture {
+class Furniture implements IEntity {
+    private String name;
 
+    Furniture(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 }
